1afbfbf9031d51e9e7b963e79e554131225d712c
timowenz <2025014@stud.hs-mannheim.de>
2023-04-03 20:36:45 +0200

    Deleted script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-let dots = window.setInterval(() => {
-    var wait = document.getElementById("wait");
-    if (wait.innerHTML.length > 2) 
-        wait.innerHTML = "";
-    else 
-        wait.innerHTML += ".";
-}, 500);
