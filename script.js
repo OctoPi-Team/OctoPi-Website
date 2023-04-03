63570ded070a111d581de1be1e59c6a4771de6bf
timowenz <2025014@stud.hs-mannheim.de>
2023-04-03 20:26:05 +0200

    Added script.js and modified index.html

---
+++ script.js
@@ -0,0 +1,7 @@
+const dots = window.setInterval(() => {
+    var wait = document.getElementById("wait");
+    if (wait.innerHTML.length > 2) 
+        wait.innerHTML = "";
+    else 
+        wait.innerHTML += ".";
+}, 500);
