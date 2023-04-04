ad860781a15f78d4b008dddf9ff0ece6870d53e7
timowenz <2025014@stud.hs-mannheim.de>
2023-04-05 00:14:37 +0200

    Refactored

--- script-dark-mode.js
+++ script-dark-mode.js
@@ -9,11 +9,17 @@ dark_mode_button.addEventListener("click", () => {
     }
 })
 
+const dark_mode_button1 = document.getElementById("dark_mode_button1");
+dark_mode_button1.addEventListener("click", () => {
+    if(!document.body.classList.contains("dark_mode")) {
+        document.body.classList.add("dark_mode");
+        localStorage.setItem("mode", "dark");
+    } else {
+        document.body.classList.remove("dark_mode");
+        localStorage.removeItem("mode", "dark");
+    }
+})
+
 if (localStorage.getItem("mode") === "dark") {
     document.body.classList.add("dark_mode");
 }
-
-const alert = document.getElementById("button-file");
-alert.addEventListener("click", () => {
-    window.alert("Not yet implemented");
-})
