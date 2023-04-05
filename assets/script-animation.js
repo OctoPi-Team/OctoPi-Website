8d194983a35abc3f4b329005d9bd8991610621ff
timowenz <2025014@stud.hs-mannheim.de>
2023-04-05 13:50:28 +0200

    Removed everything

--- assets/script-animation.js
+++
@@ -1,7 +0,0 @@
-let string = "Hi, wir sind Team Octo";
-let str = string.split("");
-let el = document.getElementById('header-animation');
-(function animate() {
-str.length > 0 ? el.innerHTML += str.shift() : clearTimeout(running); 
-let running = setTimeout(animate, 90);
-})();
