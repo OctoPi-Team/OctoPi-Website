d8d8066fa35fbf9f32b6c058e701fcb0dff24f1f
timowenz <2025014@stud.hs-mannheim.de>
2023-04-28 16:32:11 +0200

    Prettier Code was applied

--- js/script-animation.js
+++ js/script-animation.js
@@ -1,7 +1,7 @@
 let string = "Hi, wir sind Team Octo";
 let str = string.split("");
-let el = document.getElementById('header-animation');
+let el = document.getElementById("header-animation");
 (function animate() {
-str.length > 0 ? el.innerHTML += str.shift() : clearTimeout(running); 
-let running = setTimeout(animate, 90);
+  str.length > 0 ? (el.innerHTML += str.shift()) : clearTimeout(running);
+  let running = setTimeout(animate, 90);
 })();
--- js/script-dark-mode.js
+++ js/script-dark-mode.js
@@ -1,25 +1,25 @@
 const dark_mode_button = document.getElementById("dark_mode_button");
 dark_mode_button.addEventListener("click", () => {
-    if(!document.body.classList.contains("dark_mode")) {
-        document.body.classList.add("dark_mode");
-        localStorage.setItem("mode", "dark");
-    } else {
-        document.body.classList.remove("dark_mode");
-        localStorage.removeItem("mode", "dark");
-    }
-})
+  if (!document.body.classList.contains("dark_mode")) {
+    document.body.classList.add("dark_mode");
+    localStorage.setItem("mode", "dark");
+  } else {
+    document.body.classList.remove("dark_mode");
+    localStorage.removeItem("mode", "dark");
+  }
+});
 
 const dark_mode_button1 = document.getElementById("dark_mode_button1");
 dark_mode_button1.addEventListener("click", () => {
-    if(!document.body.classList.contains("dark_mode")) {
-        document.body.classList.add("dark_mode");
-        localStorage.setItem("mode", "dark");
-    } else {
-        document.body.classList.remove("dark_mode");
-        localStorage.removeItem("mode", "dark");
-    }
-})
+  if (!document.body.classList.contains("dark_mode")) {
+    document.body.classList.add("dark_mode");
+    localStorage.setItem("mode", "dark");
+  } else {
+    document.body.classList.remove("dark_mode");
+    localStorage.removeItem("mode", "dark");
+  }
+});
 
 if (localStorage.getItem("mode") === "dark") {
-    document.body.classList.add("dark_mode");
+  document.body.classList.add("dark_mode");
 }
